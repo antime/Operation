a6476b32031afe3df7631aa6aadb2d21ce4ff2e3
an_time <an_time@foxmail.com>
2018-01-29 17:18:33 +0800

    add sites

---
+++ js/data.js
@@ -0,0 +1,218 @@
+/* 
+* @Subject Author by: luuman
+* @Content Author by:   Coco413
+* @Content Author by:   an_time
+* @Date:   2017-03-02 23:10:45
+* @Function: Navigation
+* @Useage:
+*  "Name":"name"
+*  "Title":"title"
+*  "Href":"htttp:"
+*  "New":"new","New":"1"
+*/
+var FrontGuide = {
+    "main":[{
+        "pageT1":"安全圈",
+        "pageC1":[{
+            "pageT2":"安全门户",
+            "pageC2":[
+                {'Name':'安全周','Title':'网络安全的资讯，漏洞，招聘，沙龙等信息报道','Href':'https://www.secz.org/',"New":"1"},
+
+            ]
+            },{
+            "pageT2":"安全维基",
+            "pageC2":[
+                {'Name':'乌云漏洞平台','Title':'与其听信谣言,不如相信乌云','Href':'http://wooyun.chamd5.org'},
+            ]
+            },{
+            "pageT2":"国家安全",
+            "pageC2":[
+                {'Name':'CVERC','Title':'国家计算机病毒应急处理中心','Href':'http://www.cverc.org.cn/'},
+            ]
+            },{
+            "pageT2":"漏洞平台",
+            "pageC2":[
+                {'Name':'乌云','Title':'乌云-漏洞报告平台','Href':'http://www.wooyun.org/'},
+            ]
+            },{
+            "pageT2":"漏洞信息",
+            "pageC2":[
+                {'Name':'CVE中文漏洞信息库','Title':'CVE 通用漏洞与披露','Href':'http://cve.scap.org.cn/'},
+            ]
+            },{
+            "pageT2":"应急响应",
+            "pageC2":[
+                {'Name':'腾讯SRC','Title':'腾讯安全应急响应中心','Href':'https://security.tencent.com/'}
+
+            ]
+            },{
+            "pageT2":"CTF",
+            "pageC2":[
+                {'Name':'CTFtools','Title':'致力于为CTFer提供便利','Href':'https://www.ctftools.com/'}
+            ]
+        }]},{
+        "pageT1":"在线教育",
+        "pageC1":[{
+            "pageT2":"在线学习",
+            "pageC2":[
+                {'Name':'i春秋','Title':'培育信息时代的安全感','Href':'http://www.ichunqiu.com/'}
+            ]
+            },{
+            "pageT2":"在线靶机",
+            "pageC2":[
+                {'Name':'VulApps','Title':'快速搭建各种漏洞环境@Medicean','Href':'https://github.com/Medicean/VulApps'}
+            ]
+            },{
+            "pageT2":"思维导图",
+            "pageC2":[
+                {'Name':'安全技能路线 - SecWiki','Title':'安全技能 -SecWiki','Href':'https://www.sec-wiki.com/skill/index?navisec'}
+            ]
+        }]},{
+        "pageT1":"态势感知",
+        "pageC1":[{
+            "pageT2":"威胁情报",
+            "pageC2":[
+                {'Name':'360威胁情报中心','Title':'威胁情报中心','Href':'https://ti.360.com/'}
+
+            ]
+            },{
+            "pageT2":"情报收集",
+            "pageC2":[
+                {'Name':'Twitter','Title':'推特情报','Href':'https://twitter.com/'}
+            ]
+        }]},{
+        "pageT1":"代码编程",
+        "pageC1":[{
+            "pageT2":"Python",
+            "pageC2":[
+                {'Name':'V2EX','Title':'V2EX 是一个关于分享和探索的地方','Href':'https://www.v2ex.com/go/python'}
+
+            ]
+            },{
+            "pageT2":"Php",
+            "pageC2":[
+                {'Name':'PHP手册','Title':'Php官方手册','Href':'http://www.php.net/manual/zh/'},
+            ]
+            },{
+            "pageT2":"Javascript",
+            "pageC2":[
+                {'Name':'JavaScript中文网','Title':'Javascript教程资源分享门户','Href':'http://www.javascriptcn.com/'}
+            ]
+        }]},{
+        "pageT1":"文档/资源库",
+        "pageC1":[{
+            "pageT2":"资料文档",
+            "pageC2":[
+                {'Name':'POC编写指南','Title':'POC编写指南','Href':'https://poc.evalbug.com/chapter1/1.html'}
+
+            ]
+            },{
+            "pageT2":"资源下载",
+            "pageC2":[
+                {'Name':'MSDN','Title':'MSDN 我告诉你','Href':'http://itellyou.cn/'}
+            ]
+            },{
+            "pageT2":"API接口",
+            "pageC2":[
+                {'Name':'JSON API免费接口','Title':'JSON API免费接口-BeJSON.com','Href':'http://www.bejson.com/'}
+            ]
+            },{
+            "pageT2":"外包站点",
+            "pageC2":[
+                {'Name':'A5站点','Title':'中国最大的互联网交易平台,域名,网站,广告交易','Href':'http://www.a5.net/'}
+
+
+            ]
+        }]},{
+        "pageT1":"设备环境",
+        "pageC1":[{
+            "pageT2":"MAC环境",
+            "pageC2":[
+                {'Name':'Awesome-mac','Title':'Mac必装工具','Href':'https://jaywcjlove.github.io/awesome-mac/'}
+
+            ]
+            },{
+            "pageT2":"其他环境",
+            "pageC2":[
+                {'Name':'利器','Title':'创造者和他们的工具','Href':'http://liqi.io/'}
+            ]
+        }]},{
+        "pageT1":"渗透辅助/平台",
+        "pageC1":[{
+            "pageT2":"社工库",
+            "pageC2":[
+                {'Name':'70Sec','Title':'70sec社工库查询系统内部版','Href':'http://s.70sec.com/'}
+
+            ]
+            },{
+            "pageT2":"指纹收集",
+            "pageC2":[
+                {'Name':'Nmap在线扫描','Title':'Nmap Online Scanner','Href':'http://nmap.online-domain-tools.com/'}
+
+
+            ]
+            },{
+            "pageT2":"渗透工具",
+            "pageC2":[
+                {'Name':'任意命令执行','Title':'在线命令执行','Href':'http://www.aisec.cn/lab/bash.php'},
+            ]
+        }]},{
+        "pageT1":"实用工具",
+        "pageC1":[{
+            "pageT2":"空间搜索",
+            "pageC2":[
+                {'Name':'Zoomeye','Title':'钟馗之眼 - 网络空间搜索引擎','Href':'https://www.zoomeye.org/'},
+
+            ]
+            },{
+            "pageT2":"IP域名",
+            "pageC2":[
+                {'Name':'IP高精定位一','Title':'IP高精度定位','Href':'https://www.opengps.cn/Data/IP/LocHighAcc.aspx'}
+
+            ]
+            },{
+            "pageT2":"编码加密",
+            "pageC2":[
+                {'Name':'MD5解密','Title':'本站对于md5、sha1、mysql、ntlm等的实时解密成功率在全球遥遥领先。成立10年，从未被超越','Href':'http://www.cmd5.com/'}
+            ]
+            },{
+            "pageT2":"办公工具",
+            "pageC2":[
+                {'Name':'文图','Title':'文图旨在为用户打造一个可在线编辑、即时生成数据报告的工具','Href':'https://wentu.io/'}
+
+            ]
+            },{
+            "pageT2":"安全监测",
+            "pageC2":[
+                {'Name':'360显危镜','Title':'360显危镜是一个基于云的移动应用安全扫描器','Href':'http://appscan.360.cn/'}
+
+            ]
+            },{
+            "pageT2":"文件安全",
+            "pageC2":[
+                {'Name':'火眼','Title':'火眼-您的文件行为分析专家！','Href':'https://fireeye.ijinshan.com/'}
+            ]
+            },{
+            "pageT2":"其他工具",
+            "pageC2":[
+                {'Name':'代码格式化','Title':'HTML、XML、CSS、 JSON、Js、Java、SQL代码格式化','Href':'http://tool.oschina.net/codeformat/java'},
+
+
+
+            ]
+        }]},{
+        "pageT1":"技术博客",
+        "pageC1":[{
+            "pageT2":"团队博客",
+            "pageC2":[
+                {'Name':'知道创宇Paper','Title':'安全技术精粹','Href':'http://paper.seebug.org/'},
+            ]
+            },{
+            "pageT2":"个人博客",
+            "pageC2":[
+                {'Name':'余弦','Title':'在宇宙的历史长河中，我们渺小到绝望。但这不应该是我们该操心的事。','Href':'http://evilcos.me/',"New":"1"},
+
+            ]
+        }],
+    },],
+}
